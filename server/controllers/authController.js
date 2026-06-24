a2e1baae8ace29ec5ec483b8d059862bcab58a6d
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-06-24 17:09:01 +0530

    Add user login API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,20 @@
 import User from '../model/user.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-export const registerUser = async(req,res)=>{
-    try{
-        const {name,email,password} = req.body;
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
 
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             return res.status(400).json({
                 message: "All fields are required",
             })
         }
 
-        const userExist = await User.findOne({email});
+        const userExist = await User.findOne({ email });
 
-        if(userExist){
+        if (userExist) {
             return res.status(400).json({
                 message: "User already exists",
             })
@@ -21,25 +22,80 @@ export const registerUser = async(req,res)=>{
 
         // hash password
 
-        const hashedPassword = await bcrypt.hash(password,10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
         const user = await User.create({
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword
         });
 
         res.status(201).json({
             message: "User register Successfully",
-            user:{
+            user: {
                 id: user._id,
                 name: user.name,
                 email: user.email
             }
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
             message: error.message,
         })
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400), json({
+                message: "Email and Password is required"
+            })
+        }
+
+        // find user
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        // compare pass
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+// jwt.sign(payload, secretKey, options);
+// A JWT has 3 parts:
+
+// header.payload.signature
+
+        // generate token
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        )
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
     }
 }
